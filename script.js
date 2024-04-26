5599d0b8a94dd1af864403d5fda50eeb750a3962
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-04-26 14:11:24 +0200

    testade att lägga till en buildUI funktion och fixade en bugg

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ let userInput;
 const rankedBtn = document.getElementById('ranked');
 const popularBtn = document.getElementById('popular');
 
+// funktion som skapar UI baserat på URL som tas in i parameter
+async function buildUI(url){
+const movieData = await fetchData(url);
+alert(movieData.title);
+console.log(movieData);
+console.log(movieData.title);
+}
+
 
 async function fetchData(url){
     try{
@@ -13,10 +21,9 @@ async function fetchData(url){
         //hämtar data till json
         if(response.ok){
             const data = await response.json();
-            //returnerar när metoden kallas
-            console.log(data);
-            alert(data);
+            //returnerar datan baserat på url när metoden kallas
             return data;
+            console.log(data);
         }
     
 
@@ -26,7 +33,7 @@ async function fetchData(url){
 }
 
 rankedBtn.addEventListener('click', async function(){
-    fetchData(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`);
+    buildUI(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`);
 })
 popularBtn.addEventListener('click', async function(){
     fetchData(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
