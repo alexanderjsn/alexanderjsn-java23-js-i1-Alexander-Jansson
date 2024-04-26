6cb36febee0421f8487351371c2218c2f66224d5
alexanderjsn <109749445+alexanderjsn@users.noreply.github.com>
2024-04-26 21:10:35 +0200

    la till så sök resultat visar alla möjliga rsultat

--- movies.js
+++ movies.js
@@ -12,16 +12,63 @@ const popularBtn = document.getElementById('popular');
 //div för filmerna
 const movieHeader = document.getElementById('movie-header');
 
-
+async function buildUIQuery(url){
+    movieHeader.innerHTML = '';
+    const movieData = await fetchData(url);
+    if(movieData.total_results === 0){
+        let error = document.createElement('h2');
+        error.id = 'error';
+        error.innerText = 'Nothing found. Try searching for something else';
+        movieHeader.appendChild(error);
+}else{
+    movieData.results.forEach(movie => {    
+        let h2 = document.createElement('h2');
+        h2.innerText = movie.title || movie.name
+        h2.id = 'title';
+        movieHeader.appendChild(h2);
+        // Skapar h3 element (release date)
+        let h3 = document.createElement('h3');
+        h3.innerText = movie.release_date || movie.known_for_department;
+        h3.id = 'release-date';
+        movieHeader.appendChild(h3);
+        //skapar img element (poster)
+        let img = document.createElement('img');
+    
+        // kollar först om det finns en profile_path i datan som hämtas, tar annars poster_path
+        img.src = "https://image.tmdb.org/t/p/w500" + (movie.profile_path || movie.poster_path);
+    
+        // sätter in filmtitel eller kändisnamn som alt
+        img.alt = 'image of ' + (movie.title || movie.name);
+        img.onerror = function(){
+            // tar in placeholder bild ifall ingen bild finns
+            img.src='https://via.placeholder.com/150/0000ff/ffffff?text=No+Image+Available';
+        };
+        movieHeader.appendChild(img);
+        //skapar paragraf (beskrivning)
+        let paragraph = document.createElement('paragraph');
+        paragraph.id = 'description';
+        
+    
+        //iom att known_for är en Array så användS map för att iterera genom listan och sedan få ut titeln på varje film
+        // sedan används .join(',') för att seperera titlarna med kommatecken
+        paragraph.innerText = movie.overview || movie.known_for.map(movie => movie.title).join(', ');
+        movieHeader.appendChild(paragraph);    
+    })
+}}
 
 async function buildUI(url){
     movieHeader.innerHTML = '';
 
-    //hämtar datan 
-    
+    //hämtar datan
 const movieData = await fetchData(url);
 const tenMovies = movieData.results.slice(0,10);
+if(tenMovies.length === 0){
+    let error = document.createElement('h2');
+    error.id = 'error';
+    error.innerText = 'Nothing found. Try searching for something else';
+    movieHeader.appendChild(error);
 
+} else {
 tenMovies.forEach(movie => {    
     // Skapar h2 element (title)
     let h2 = document.createElement('h2');
@@ -60,7 +107,7 @@ tenMovies.forEach(movie => {
 
     // en rund bild top 1 av rankade filmer
 
-})}
+})}}
 rankedBtn.addEventListener('click', async function(){
     buildUI(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`);
 })
@@ -69,9 +116,9 @@ popularBtn.addEventListener('click', async function(){
 })
 searchMovieBtn.addEventListener('click', async function(){
     let searchInput = userInput.value;
-    buildUI(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchInput}`);
+    buildUIQuery(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchInput}`);
 })
 searchPersonBtn.addEventListener('click', async function(){
     let searchInput = userInput.value;
-    buildUI(`https://api.themoviedb.org/3/search/person?api_key=${apiKey}&query=${searchInput}`);
+    buildUIQuery(`https://api.themoviedb.org/3/search/person?api_key=${apiKey}&query=${searchInput}`);
 })
